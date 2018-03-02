00444867ddeb0dc7d68aabf917f608cfa84f0278
Adam Tait <adam@adamtait.com>
2018-03-01 17:36:11 -0800

    initial commit

---
+++ UIView+Style.swift
@@ -0,0 +1,27 @@
+//
+//  UIView+Style.swift
+//  pinknoise
+//
+//  Created by Adam Tait on 3/1/18.
+//  Copyright © 2018 Sisterical Inc. All rights reserved.
+//
+
+import UIKit
+
+extension UIView
+{
+    func set(style : Style)
+    {
+        if let v = style[.bgColor] as? UIColor      { self.backgroundColor = v }
+        if let v = style[.fgColor] as? UIColor      { self.tintColor = v }
+        if let v = style[.borderColor] as? UIColor  { self.layer.borderColor = v.cgColor }
+        
+        if let v = style[.borderWidth] {
+            self.layer.borderWidth = toCgFloat(anyNumber: v)
+        }
+        
+        if let v = style[.cornerRadius] {
+            self.layer.cornerRadius = toCgFloat(anyNumber: v)
+        }
+    }
+}
